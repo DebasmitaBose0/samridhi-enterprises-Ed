ec9ff7a51c412da63789622a8691f1b0ac168f2c
SRV30 <sahilrv196@gmail.com>
2026-07-23 22:51:13 +0530

    Reapply "Merge pull request #362 from DebasmitaBose0/feature/341-low-stock-alerts"
    This reverts commit 7829ea391af452810cfba4e5ef1da08e09ab3115.

--- server/route/partRoutes.js
+++ server/route/partRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import rateLimit from "express-rate-limit";
 import upload from "../middleware/multer.js";
 import {
-  addWarehouseStock,
+  getLowStockParts,
   addPart,
   getAllParts,
   getPartById,
@@ -49,6 +49,7 @@ const recommendLimiter = rateLimit({
 
 const partRouter = express.Router();
 
+partRouter.get("/admin/low-stock", auth, admin, getLowStockParts);
 partRouter.post("/add", upload.array("images", 5), auth, admin, addPart);
 partRouter.post("/warehouse-stock/:id", auth, admin, addWarehouseStock);
 partRouter.get("/get", browseLimiter, getAllParts);
