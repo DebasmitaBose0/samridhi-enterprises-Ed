4c7e0c001104e4f5e9cf77c965b7a82011a93340
SRV30 <sahilrv196@gmail.com>
2026-07-25 18:47:28 +0530

    fixed bugs

--- server/route/partRoutes.js
+++ server/route/partRoutes.js
@@ -53,7 +53,7 @@ partRouter.get("/admin/low-stock", auth, admin, getLowStockParts);
 partRouter.post("/add", upload.array("images", 5), auth, admin, addPart);
 // partRouter.post("/warehouse-stock/:id", auth, admin, addWarehouseStock);
 partRouter.get("/get", browseLimiter, getAllParts);
-partRouter.get("/search/faceted", browseLimiter, getFacetedSearch);
+// partRouter.get("/search/faceted", browseLimiter, getFacetedSearch);
 partRouter.get("/get/:id", browseLimiter, getPartById);
 partRouter.get("/get/:id/similar", browseLimiter, getSimilarParts);
 partRouter.get(
